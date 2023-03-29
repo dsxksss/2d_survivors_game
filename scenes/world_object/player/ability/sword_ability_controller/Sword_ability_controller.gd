746cc0686333b664f363cb96d93cd11cfeb2585c
VentCat <73477803+dsxksss@users.noreply.github.com>
2023-03-29 11:06:39 +0800

    实现能力升级：增加10%挥剑速度

--- scenes/world_object/player/ability/sword_ability_controller/Sword_ability_controller.gd
+++ scenes/world_object/player/ability/sword_ability_controller/Sword_ability_controller.gd
@@ -2,11 +2,15 @@ extends Node
 
 # 技能触发范围
 const MAX_RANGE = 150
+var base_wait_time
+var damage = 5
 
 @export var sword_ability:PackedScene
 
 func _ready():
+	base_wait_time = $Timer.wait_time
 	$Timer.timeout.connect(on_timer_timeout)
+	GameEvents.ability_upgrade_added.connect(on_ability_upgrade_added)
 
 func on_timer_timeout():
 	# 获取玩家node
@@ -34,7 +38,7 @@ func on_timer_timeout():
 		var b_distance = b.global_position.distance_squared_to(player.global_position)
 		return a_distance < b_distance
 	)
-		
+	
 	var sword_instance = sword_ability.instantiate() as Node2D
 	player.get_parent().add_child(sword_instance)
 	sword_instance.global_position = enemies[0].global_position
@@ -42,3 +46,15 @@ func on_timer_timeout():
 	
 	var enemy_direction = enemies[0].global_position - sword_instance.global_position
 	sword_instance.rotation = enemy_direction.angle()
+
+func on_ability_upgrade_added(upgrade:AbilityUpgrade,current_upgrades:Dictionary):
+	# 先判断是否为这个能力
+	if upgrade.id != "sword_rate":
+		return
+	var percent_reduction = current_upgrades["sword_rate"]["quantity"] * .1
+	# 缩减生成剑时间
+	$Timer.wait_time = base_wait_time * (1 - percent_reduction)
+	# 因为时间触发会一直执行，所以得重置一下时间设置
+	# 如果 time_sec > 0，则会将 wait_time 设置为 time_sec。
+	# 这也会将剩余时间重置为 wait_time。
+	$Timer.start()
