d31c6a9c0b09829e7cdf4ccdc2bbfb9dfdb12f2a
VentCat <73477803+dsxksss@users.noreply.github.com>
2023-03-30 11:25:34 +0800

    玩家受伤基本逻辑

--- scenes/world_object/player/player.gd
+++ scenes/world_object/player/player.gd
@@ -1,12 +1,18 @@
 extends CharacterBody2D
 
+@onready var damage_interval_timer = $DamageIntervalTimer
+
 # 移动速度
 const MAX_SPEED = 125
 # 加速度
 const ACCELERATION_SMOOTHING = 25
 
+var number_colliding_bodies = 0
+
 func _ready() -> void:
-	pass
+	$CollisionArea2D.body_entered.connect(on_body_entered)
+	$CollisionArea2D.body_exited.connect(on_body_exited)
+	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
 
 # 获取玩家移动偏移量
 func get_movement_vector():
@@ -24,4 +30,19 @@ func _process(delta: float) -> void:
 	# 根据 velocity 移动该物体
 	move_and_slide()
 	
+func check_deal_damage():
+	if number_colliding_bodies == 0 || !damage_interval_timer.is_stopped():
+		return
+	$HealthComponent.damage(1)
+	damage_interval_timer.start()
+	print($HealthComponent.current_health)
+	
+func on_body_entered(other_body: Node2D):
+	number_colliding_bodies += 1
+	check_deal_damage()
+	
+func on_body_exited(other_body: Node2D):
+	number_colliding_bodies -= 1	
 
+func on_damage_interval_timer_timeout():
+	check_deal_damage()
