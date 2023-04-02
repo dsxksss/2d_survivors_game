ae60d864d28a55179a806e004fcc860f3c96830f
VentCat <73477803+dsxksss@users.noreply.github.com>
2023-04-02 21:07:41 +0800

    随着时间敌人生成速度加快 修改胜利到达时间60s为300s

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,13 +1,28 @@
 extends Node
 
+# 难度递增信号
+signal arena_difficulty_increased(arena_difficulty: int)
+
+# 难度升级时间间隔(秒)
+const DIFFICULTY_INTERVAL := 5
+
 @export var end_screen_scene: PackedScene
 
-@onready var timer = $Timer
+@onready var timer:Timer = $Timer
+
+# 竞技场难度等级
+var arena_difficulty := 0
 
 func _ready() -> void:
 	timer.timeout.connect(on_timer_timeout)
 
-func get_time_elapsed():
+func _process(delta: float) -> void:
+	var next_time_target = timer.wait_time - ((arena_difficulty + 1) * DIFFICULTY_INTERVAL)
+	if timer.time_left <= next_time_target:
+		arena_difficulty += 1
+		arena_difficulty_increased.emit(arena_difficulty)
+	
+func get_time_elapsed() -> float:
 	return timer.wait_time - timer.time_left
 
 func on_timer_timeout():
--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -4,11 +4,18 @@ extends Node
 const SPAWN_RADIUS = 370
 
 @export var besic_enemy_scene: PackedScene
+@export var arena_time_manager: Node
+@onready var timer:Timer = $Timer
+var base_spawn_time: float = 0
 
 func _ready() -> void:
-	$Timer.timeout.connect(on_timer_timeout)
+	base_spawn_time = timer.wait_time
+	timer.timeout.connect(on_timer_timeout)
+	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
 
 func on_timer_timeout():
+	timer.start()
+	
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if player == null:
 		return
@@ -21,3 +28,9 @@ func on_timer_timeout():
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
 	entities_layer.add_child(enemy)
 	enemy.global_position = spawn_position
+
+func on_arena_difficulty_increased(arena_difficulty: int):
+	var time_offset = (.1 / 12) * arena_difficulty
+	time_offset = min(time_offset, .7)
+	print(time_offset)
+	timer.wait_time = base_spawn_time - time_offset
